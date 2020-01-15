95e26e01549dde60ad57ed0c29a24bf7c0b15c16
Julian Rutten <julian.rutten@gmail.com>
2020-01-15 23:39:43 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# convential-commits
-Tools and Cloudbuild containers for convential commits.
+# Conventional commit tag generator
+
+Generates tags based on commits. Uses convential commit specification. See [conventialcommits.org](https://www.conventionalcommits.org/en/v1.0.0/) for a detailed specification. Only the basics will be outlined here
+
+## Basics
+This script generates the next tag version based on commits messages since last tag (version) to determine if either the major, minor or patch need to be incremented. The messages should contain a `type` (see below).
+
+### Patch
+`fix:` or `improvement:` increments the patch version
+
+### Minor
+`feat:` or `performance:` increments the minor version. `feature:` is also accepted.
+
+### Major / Breaking changes
+`!` after the previously mentioned `type` denotes a breaking change. For example `fix!:`. This increments the major version.
+
+### Non-impacting commit messages
+Some commits don't need to change the version. Examples of types to use in such cases include but are not limited to
+`build:`, `chore:`, `ci:`, `docs:`, `style:`, `refactor:`, `perf:`, `test:`
+
+## Usage
+```sh
+./generate-tag.sh
+```
+
+## Todo
+Include docker/cloudbuild files. Any contributions or comments are welcome!
---
+++ generate-tag.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+TAG=$(git describe HEAD)
+VERSION=$(echo "$TAG" | grep -o '[0-9]*\.[0-9]*\.[0-9]*')
+
+if [ -z "$TAG" ] || [ -z "$VERSION" ] ; then
+  #Not a valid version
+  echo "untagged";
+  exit 0;
+fi
+
+MESSAGES=$(git log $VERSION..HEAD --pretty=oneline --abbrev-commit | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')
+COUNT=0
+MAJOR=$(echo "$VERSION" | cut -d'.' -f1)
+MINOR=$(echo "$VERSION" | cut -d'.' -f2)
+PATCH=$(echo "$VERSION" | cut -d'.' -f3)
+LEVEL=3
+function setLevel(){
+  [[ $1 < $LEVEL ]] && LEVEL=$1
+}
+
+while read -r line; do
+  if [[ $(echo "$line" | grep -E '(improvement|fix|feature|feat|performance)\!:') ]] || [[ $(echo "$line" | grep 'BREAKING CHANGE:') ]]; then
+    setLevel 0
+    ((MAJOR=MAJOR+1))
+    break
+  fi
+  if [[ $(echo "$line" | grep -E 'improvement:|fix:') ]]; then
+    ((MINOR=MINOR+1))
+    setLevel 1
+  fi
+  if [[ $(echo "$line" | grep -E 'feature:|feat:|performance:') ]]; then
+    ((PATCH=PATCH+1))
+    setLevel 2
+  fi
+  ((COUNT=COUNT+1))
+done <<< "$MESSAGES"
+
+#No bumps required, returning current tag
+if [ $LEVEL == 3 ]; then
+  echo "$VERSION"
+  exit 0;
+fi
+
+#Reset minor or patch if the parent got incremented
+if [ "$LEVEL" -le 1 ]; then
+  PATCH=0
+fi
+if [ $LEVEL == 0 ]; then
+  MINOR=0
+fi
+
+echo "$MAJOR.$MINOR.$PATCH-rc.$COUNT"
